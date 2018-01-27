c5b8a5e62854397b81de8594499c1c6465a29462
Quasar-light <t.d.sande@gmail.com>
2018-01-27 16:18:58 +0100

    Add files via upload

---
+++ html/play.js
@@ -0,0 +1,35 @@
+
+<script> 
+import { JsonObject } from "./jsonobject";
+import { QuestionFactory } from "./questionfactory";
+import { QuestionCheckboxBase } from "./question_baseselect";
+
+
+export class QuestionRadiogroupModel extends QuestionCheckboxBase {
+  constructor(public name: string) {
+    super(name);
+  }
+  public getType(): string {
+    return "radiogroup";
+  }
+  supportGoNextPageAutomatic() {
+    return true;
+  }
+}
+
+JsonObject.metaData.addClass(
+  "radiogroup",
+  [],
+  function() {
+    return new QuestionRadiogroupModel("");
+  },
+  "checkboxbase"
+);
+
+QuestionFactory.Instance.registerQuestion("radiogroup", name => {
+  var q = new QuestionRadiogroupModel(name);
+  q.choices = QuestionFactory.DefaultChoices;
+  return q;
+});
+
+</script>
